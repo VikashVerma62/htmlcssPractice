0051e7ec97c0fdfc370c5e4d6ca51eb9133c1867
VikashVerma62 <vikashverma626506@gmail.com>
2025-04-18 18:20:28 -0700

    update

--- first.js
+++ first.js
@@ -1,3 +1,13 @@
  console.log("hello world");
  console.log("hello dunia");
- 
+
+//  variable is a named memory location which we give a name
+// var keyword is function scoped.
+// var is redeclered variable.
+
+// let is block scoped  which is not used outside the block and redefination is not allowed in js.
+let a=10;
+console.log(a);
+
+// const means const value which value is not changed
+
---
+++ operaterAndConditionls.js
@@ -0,0 +1,43 @@
+  console.log();
+  
+  // bitwise operater
+  // And OR Not << 
+  console.log(2 & 5);
+  console.log(2 | 5);
+  console.log(~(2))
+  console.log(2^2);
+   
+
+
+  let number=5;
+  if(number==1){
+    console.log('A');
+    
+  }
+  else if(number==2){
+    console.log('B');
+  }
+  else if(number==3){
+    console.log('C');
+  }
+  else if(number==4){
+    console.log('D');
+  }
+  else if(number==5){
+    console.log('E');
+  }
+
+  let num=3;
+  switch(num){
+    case 1: console.log('A');
+    break;
+    case 2: console.log('B');
+    break;
+    case 3: console.log('C'); 
+    break;
+    case 4: console.log('D');
+    break;
+    case 5: console.log('E');
+    break; 
+    
+  }
