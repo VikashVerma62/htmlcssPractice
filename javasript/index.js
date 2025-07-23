5c20bc0e315a814d614886dbee3a5bd373326d6b
VikashVerma62 <vikashverma626506@gmail.com>
2025-07-22 21:43:53 -0700

    update

--- javasript/index.js
+++ javasript/index.js
@@ -122,3 +122,61 @@ let getExp=(x,y)=>{
 
 }
 console.log(getExp(2,10));
+
+
+let arr=[1,2,"love",true,];
+arr.push("vikash");
+console.log(arr);
+arr.pop();
+console.log(arr);
+arr.shift();
+console.log(arr);
+arr.unshift(1);
+console.log(arr);
+arr.push(5)
+arr.push(6);
+console.log(arr);
+console.log(arr.slice(2,5));
+
+console.log(arr.splice(1,2,'kunal'));
+console.log(arr)
+let arr1=[10,20,30];
+let ans=arr1.map((number)=>{
+   return number*number;
+})
+console.log(ans);
+
+
+
+let arr2=[1,2,"hello","wow"];
+let ans1=arr.filter((value)=>{
+if(typeof(value)==='number'){
+   return true;
+}
+else{
+   return false;
+}
+})
+
+console.log(ans1);
+
+
+let arr3=[7,5,4,3,2,1];
+arr3.sort();
+console.log(arr3)
+console.log(arr3.indexOf(3))
+arr3.forEach((number,index)=>{
+   console.log("Number",number ,"index",index);
+})
+
+let arr4=[1,2,3,4,5]
+let letSum=(arr4)=>{
+   let sum=0;
+  let len=arr4.length;
+  for(let i=0; i<len; i++){
+   sum=sum+i;
+   return sum;
+  }
+}
+totalsum=letSum(arr4);
+console.log(letSum);
